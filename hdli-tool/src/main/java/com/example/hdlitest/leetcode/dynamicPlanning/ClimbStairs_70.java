d9aeee9384357db2bab83817dbff6e6ed942671b
luyi.lhd <luyi.lhd@alibaba-inc.com>
2024-01-25 01:34:24 +0800

    动态规划2024.01.24

--- hdli-tool/src/main/java/com/example/hdlitest/leetcode/dynamicPlanning/ClimbStairs_70.java
+++ hdli-tool/src/main/java/com/example/hdlitest/leetcode/dynamicPlanning/ClimbStairs_70.java
@@ -29,4 +29,19 @@ public class ClimbStairs_70 {
         }
         return dp[n];
     }
+
+    public int climbStairs2(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+        for(int i = 3 ;i<= n;i++){
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
 }
