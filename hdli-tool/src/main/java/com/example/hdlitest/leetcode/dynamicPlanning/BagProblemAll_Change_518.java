d782c8986a6718df28e9de501f529d5812019fc4
luyi.lhd <luyi.lhd@alibaba-inc.com>
2024-02-01 01:47:44 +0800

    01背包问题-2024.01.31

---
+++ hdli-tool/src/main/java/com/example/hdlitest/leetcode/dynamicPlanning/BagProblemAll_Change_518.java
@@ -0,0 +1,32 @@
+package com.example.hdlitest.leetcode.dynamicPlanning;
+
+/**
+ * 完全背包
+ * @author luyi
+ * @date 2024/2/1 01:24
+ */
+public class BagProblemAll_Change_518 {
+    /**
+     * 零钱兑换 II：给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+     *
+     * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+     *
+     * 假设每一种面额的硬币有无限个。
+     *
+     *
+     *
+     * @param amount
+     * @param coins
+     * @return
+     */
+    public int change(int amount, int[] coins) {
+        int [] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int i = 0;i< coins.length;i++){
+            for(int j = coins[i];j <= amount ;j++){
+                dp[j] += dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+}
