8286b0676ab3abef9c03de2ec6ee4c0354177433
luyi.lhd <luyi.lhd@alibaba-inc.com>
2024-02-16 00:37:33 +0800

    动态规划子序列-2024.02.015

--- hdli-tool/src/main/java/com/example/hdlitest/leetcode/dynamicPlanning/Zixulie4_1143.java
+++ hdli-tool/src/main/java/com/example/hdlitest/leetcode/dynamicPlanning/Zixulie4_1143.java
@@ -17,6 +17,7 @@ public class Zixulie4_1143 {
      * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
      *
      *
+     *
      * 本题和动态规划：718. 最长重复子数组 (opens new window)区别在于这里不要求是连续的了，
      * 但要有相对顺序，即："ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
      * @param text1
@@ -31,8 +32,10 @@ public class Zixulie4_1143 {
         for (int i =1;i<= char1.length;i++){
             for (int j = 1;j<=char2.length;j++){
                 if (char1[i-1] == char2[j-1]){
+                    //连续
                     dp[i][j] = dp[i-1][j-1]+1;
                 }else {
+                    //不连续的情况
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
