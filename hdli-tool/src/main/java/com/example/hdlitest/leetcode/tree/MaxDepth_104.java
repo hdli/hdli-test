f972545416e787f1aeb3429c7ab53d6426ced894
luyi.lhd <luyi.lhd@alibaba-inc.com>
2024-03-12 00:48:13 +0800

    二叉树：0311

--- hdli-tool/src/main/java/com/example/hdlitest/leetcode/tree/MaxDepth_104.java
+++ hdli-tool/src/main/java/com/example/hdlitest/leetcode/tree/MaxDepth_104.java
@@ -1,7 +1,7 @@
 package com.example.hdlitest.leetcode.tree;
 
 /**
- * 二叉树的最大深度
+ * 二叉树的最大深度:
  * @author luyi
  * @date 2023/4/30 4:22 PM
  */
@@ -21,9 +21,32 @@ public class MaxDepth_104 {
         return Math.max(l,r)+1;
     }
 
+    /**
+     * 前序遍历 + 回溯
+     */
+    int maxDepth = 0;
+    public void qianxu(TreeNode node,int depth){
+        if (node.left == null && node.right == null){
+            maxDepth = Math.max(maxDepth,depth);
+            return;
+        }
+        if (node.left != null){
+            qianxu(node.left,depth+1);
+        }
+        if (node.right != null){
+            qianxu(node.right,depth+1);
+        }
+    }
+
+
+
+
+
+
 
     /**
      * 二叉树的最小深度
+     * 后序遍历
      * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量
      * @param root
      * @return
@@ -32,10 +55,11 @@ public class MaxDepth_104 {
         if(root == null){
             return 0;
         }
+        //后序遍历
         int ld = min(root.left);
         int rd = min(root.right);
 
-        //最小深度是从根节点到最近叶子节点的最短路径上的节点数量
+        //处理特殊情况
         if(root.left == null && root.right != null){
             return 1+rd;
         }
@@ -43,6 +67,27 @@ public class MaxDepth_104 {
             return 1+ld;
         }
         return 1+Math.min(ld,rd);
+    }
+
+
+    int minDepth = Integer.MAX_VALUE;
+    /**
+     * 二叉树的最小深度 ： 前序遍历 + 回溯
+     * @param node
+     * @return
+     */
+    public void minDepth(TreeNode node,int depth) {
+        if (node.left == null && node.right == null){
+            minDepth = Math.min(minDepth,depth);
+            return;
+        }
+        if (node.left != null){
+            minDepth(node.left,depth+1);
+        }
+        if (node.right != null){
+            minDepth(node.right,depth+1);
+        }
+
     }
 
 
